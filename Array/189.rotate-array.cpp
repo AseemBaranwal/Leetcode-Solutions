6123e3c4289013b7f24b30c4780630d84dd7a5e3
Aseem Baranwal <aseembaranwal23@gmail.com>
2022-03-20 22:16:21 +0530

    Rotate Array to right by k in O(1) space

--- Array/189.rotate-array.cpp
+++ Array/189.rotate-array.cpp
@@ -8,14 +8,17 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        // Space Complexity: O(1) - 2 Pass O(N) solution
         int n = nums.size();
-        vector<int> auxillary(n);
-        for(int i = 0; i < n; i++) {
-            int newIndex = (i+k)%n;
-            auxillary[newIndex] = nums[i];
-        }
-        for(int i = 0; i < n; i++)
-            nums[i] = auxillary[i];
+        k %= n;
+        reverse(nums, 0, n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+private:
+    void reverse(vector<int>& nums, int start, int end) {
+        while(start < end)
+            swap(nums[start++], nums[end--]);
     }
 };
 // @lc code=end
